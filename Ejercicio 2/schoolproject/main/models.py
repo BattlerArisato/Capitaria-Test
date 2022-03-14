f33bd6f195e8f04ff7a00aeb89650c2135c1f8b0
Fabian <fabian@movatec.cl>
2022-03-14 12:49:37 -0300

    base del proyecto y modelos

--- Ejercicio 2/schoolproject/main/models.py
+++ Ejercicio 2/schoolproject/main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    ID_Student = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    ID_Teacher = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    ID_Course = models.CharField(max_length=10)
+    ID_Teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Exam(models.Model):
+    ID_Exam = models.CharField(max_length=10)
+    ID_Student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    ID_Course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    Score = models.IntegerField()
+
+    def __str__(self):
+        return self.ID_Exam
--- Ejercicio 2/schoolproject/main/views.py
+++ Ejercicio 2/schoolproject/main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def index(response):
+    return render(response, 'main/index.html', {})
